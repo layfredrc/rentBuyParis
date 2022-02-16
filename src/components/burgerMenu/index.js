2e507e128e2ac02b9c6e2f84b060bf5acde43f7c
vincent <vincent.n@hotmail.fr>
2022-02-16 18:27:03 +0100

    [BURGER MENU] animations and css

--- src/components/burgerMenu/index.js
+++ src/components/burgerMenu/index.js
@@ -1,5 +1,6 @@
 import styles from "./index.module.scss";
-import { MdClose, MdLanguage, MdAttachMoney } from "react-icons/md";
+import { MdClose, MdLanguage, MdAttachMoney, MdPhone } from "react-icons/md";
+import Link from "next/link";
 
 export default function BurgerMenu({ setIsBurgerMenuOpened }) {
   return (
@@ -7,33 +8,76 @@ export default function BurgerMenu({ setIsBurgerMenuOpened }) {
       <header className={styles.header}>
         <div className={styles.left}>
           <MdClose size={25} onClick={() => setIsBurgerMenuOpened(false)} />
-          <p style={{ fontSize: 20 }}>RentBuyParis</p>
+          <p>RentBuyParis</p>
         </div>
         <div className={styles.right}>
-          <MdLanguage size={25} onClick={() => setIsBurgerMenuOpened(false)} />
+          <MdLanguage size={20} onClick={() => setIsBurgerMenuOpened(false)} />
           <p>Français</p>
           <MdAttachMoney
-            size={25}
+            size={20}
             onClick={() => setIsBurgerMenuOpened(false)}
           />
           <p>EUR</p>
         </div>
       </header>
-      <p>INFORMATION</p>
-      <p>Accueil</p>
-      <p>Location d’appartement</p>
-      <p>Biens à vendre / chasser</p>
-      <p>Terms and conditions</p>
-      <h1>rentBuyParis</h1>
-      <h2>Stephan Jaquet</h2>
-      <div></div>
-      <div></div>
-      <div></div>
-      <p>Please send a SMS if I am not answering</p>
-      <p>
-        47 rue de Laborde, 75008 Paris Mon – Fri: 11:00 — 18:00 (CET), Sat –
-        Sun: available for a call
-      </p>
+      <div className={styles.content}>
+        <div className={styles.navigation}>
+          <p className={styles.info}>Information</p>
+          <Link href="/">
+            <a
+              className={styles.link}
+              onClick={() => setIsBurgerMenuOpened(false)}
+            >
+              Accueil
+            </a>
+          </Link>
+          <Link href="/accomodations">
+            <a
+              className={styles.link}
+              onClick={() => setIsBurgerMenuOpened(false)}
+            >
+              Location d’appartement
+            </a>
+          </Link>
+          <Link href="/accomodations">
+            <a
+              className={styles.link}
+              onClick={() => setIsBurgerMenuOpened(false)}
+            >
+              Biens à vendre / chasser
+            </a>
+          </Link>
+          <Link href="/">
+            <a
+              className={styles.link}
+              style={{ marginTop: "50px" }}
+              onClick={() => setIsBurgerMenuOpened(false)}
+            >
+              Terms and conditions
+            </a>
+          </Link>
+        </div>
+        <div className={styles.contact}>
+          <p className={styles.brand}>RentBuyParis</p>
+          <h2 style={{ fontSize: "20px", fontWeight: "700" }}>
+            Stephan Jaquet
+          </h2>
+          <div className={styles.information}>
+            <MdPhone size={20} />
+            <span>+33 6 72 64 44 44</span>
+          </div>
+          <div className={styles.information}>
+            <MdPhone size={20} />
+            <span> info@rentbuyparis.com</span>
+          </div>
+          <div></div>
+          <p>Please send a SMS if I am not answering</p>
+          <p>
+            47 rue de Laborde, 75008 Paris Mon – Fri: 11:00 — 18:00 (CET), Sat –
+            Sun: available for a call
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
