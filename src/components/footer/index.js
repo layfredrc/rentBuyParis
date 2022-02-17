de6a642588a70e70a770ab22ce347ca42f4dd100
vincent <vincent.n@hotmail.fr>
2022-02-17 15:20:53 +0100

    [FOOTER] improve display

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -5,41 +5,51 @@ import { MdLocationPin, MdOutlinePhone } from "react-icons/md";
 import styles from "./index.module.scss";
 
 export default function Footer() {
-  return (
-    <footer className={styles["container"]}>
-      <div>
-        <div>
-          <h2 className={styles["logo"]}>RentBuyParis</h2>
-          <div>
-            <MdLocationPin size={20} />
-            <p style={{ marginBlock: "auto" }}>47 rue de Laborde 75008 Paris</p>
-          </div>
-          <div>
-            <MdOutlinePhone size={20} />
-            <p>+33672644444</p>
-          </div>
-        </div>
-        <div>
-          <h2>Horaires</h2>
-          <p>Lundi - Vendredi</p>
-          <p>11h - 23h</p>
-        </div>
-        <div>
-          <h2>Plan du site</h2>
-          <Link href="/">
-            <a>Louer un appartement</a>
-          </Link>
-          <Link href="/">
-            <a>Bien à vendre</a>
-          </Link>
-          <Link href="/">
-            <a>Termes et conditions</a>
-          </Link>
-        </div>
-      </div>
-      <div>
-        <p>©RentBuyParis, Stephan Jaquet. All right reserved.</p>
-      </div>
-    </footer>
-  );
+	return (
+		<footer className={styles["container"]}>
+			<div>
+				<div>
+					<h2 className={styles["logo"]}>RentBuyParis</h2>
+					<div>
+						<MdLocationPin size={20} />
+						<p style={{ marginBlock: "auto" }}>47 rue de Laborde 75008 Paris</p>
+					</div>
+					<div>
+						<MdOutlinePhone size={20} />
+						<p>+33672644444</p>
+					</div>
+				</div>
+				<div>
+					<h2>Horaires</h2>
+					<div>
+						<p>Lundi - Vendredi</p>
+					</div>
+					<div>
+						<p>11h - 23h</p>
+					</div>
+				</div>
+				<div>
+					<h2>Plan du site</h2>
+					<div>
+						<Link href="/">
+							<a>Louer un appartement</a>
+						</Link>
+					</div>
+					<div>
+						<Link href="/">
+							<a>Bien à vendre</a>
+						</Link>
+					</div>
+					<div>
+						<Link href="/">
+							<a>Termes et conditions</a>
+						</Link>
+					</div>
+				</div>
+			</div>
+			<div>
+				<p>©RentBuyParis, Stephan Jaquet. All right reserved.</p>
+			</div>
+		</footer>
+	);
 }
