a70782a0663ab02c4ca0251318adc90ec287b70c
vincent <vincent.n@hotmail.fr>
2022-02-18 16:11:23 +0100

    [CARD] new component

--- src/components/accomodationsHub/index.js
+++ src/components/accomodationsHub/index.js
@@ -1,10 +1,31 @@
 import CalendarDates from "../calendar";
+import HorizontalCard from "../card/horizontal";
 import styles from "./index.module.css";
+
+const data = {
+	src: "https://images.unsplash.com/photo-1502672260266-1c1ef2d93688?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1980&q=80",
+	name: "Joséphine",
+	arrondissement: "15",
+	street: "10 rue de Rivoli 75015",
+	isDisponible: true,
+	nbRooms: 2,
+	size: 30,
+	price: 1500,
+};
 
 export default function AccomodationsHub() {
 	return (
 		<div className={styles.container}>
-			<CalendarDates title="Quand voulez-vous venir ?" />
+			<div></div>
+			<div className={styles.main}>
+				<CalendarDates title="Quand voulez-vous venir ?" />
+				<div className={styles.content}>
+					<div className={styles.filters}></div>
+					<div className={styles.accomodations}>
+						<HorizontalCard data={data} />
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }
