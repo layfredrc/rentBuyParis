fd2dee3e6caab1ff3217e07833bb5a94977987a2
vincent <vincent.n@hotmail.fr>
2022-02-18 20:07:25 +0100

    [VERTICAL CARD] new component

---
+++ src/components/card/vertical/index.js
@@ -0,0 +1,62 @@
+import Image from "next/image";
+
+import {
+	RiMapPin2Line,
+	RiCheckboxCircleLine,
+	RiHotelBedFill,
+} from "react-icons/ri";
+import { GiResize } from "react-icons/gi";
+import Button from "../../button";
+import Apartment from "../../../../public/assets/images/image 2.png";
+
+import styles from "./index.module.scss";
+
+export default function VerticalCard({ data }) {
+	return (
+		<div className={styles["container"]}>
+			<Image src={Apartment} />
+			<div>
+				<div>
+					<div>
+						<p>
+							<span>{data.name}</span>, {data.arrondissement}ème arrondissement
+						</p>
+					</div>
+					<div>
+						<RiMapPin2Line size={20} style={{ marginRight: "5px" }} />
+						<span>{data.street}</span>
+					</div>
+					<div className={styles.details}>
+						<div style={{ color: "#15895F" }}>
+							<RiCheckboxCircleLine color="#15895F" size={20} />
+							<span>{data.isDisponible ? "Disponible" : "Indisponible"}</span>
+						</div>
+						<div>
+							<RiHotelBedFill size={20} />
+							<span>{data.nbRooms} chambres</span>
+						</div>
+						<div>
+							<GiResize size={20} />
+							<span> {data.size} m²</span>
+						</div>
+					</div>
+				</div>
+				<div style={{ display: "flex", justifyContent: "space-between" }}>
+					<div>
+						<span
+							style={{
+								fontSize: "20px",
+								fontWeight: "600",
+								marginRight: "5px",
+							}}
+						>
+							{data.price}€
+						</span>
+						par mois
+					</div>
+					<Button title="Réserver" />
+				</div>
+			</div>
+		</div>
+	);
+}
