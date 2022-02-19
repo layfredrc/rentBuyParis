41638dc449fbbfd43888ab493cb4e4e5f3b19c4a
vincent <vincent.n@hotmail.fr>
2022-02-19 16:01:39 +0100

    [BUTTON] modified

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -2,10 +2,14 @@ import { DateRangePicker, DatePicker } from "@mantine/dates";
 import { useState } from "react";
 import styles from "./index.module.scss";
 
-export default function Button({ title }) {
+export default function Button({ title, icon, padding }) {
 	return (
-		<div className={styles.container}>
-			<p>{title}</p>
+		<div
+			className={styles.container}
+			style={padding ? { padding: padding } : {}}
+		>
+			{icon ? icon : null}
+			<p style={icon && { marginLeft: "5px" }}>{title}</p>
 		</div>
 	);
 }
