10b1c77bdd1cbb8c7f4d7b7ae0c50af3b1b50f10
vincent <vincent.n@hotmail.fr>
2022-02-19 16:02:49 +0100

    [BurgerMenu] disable scroll when opened

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,22 +1,28 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import BurgerMenu from "../components/burgerMenu";
 import Footer from "../components/footer";
 import Header from "../components/header";
 import "../styles/globals.scss";
+import { useScrollLock } from "@mantine/hooks";
 
 function MyApp({ Component, pageProps }) {
-  const [isBurgerMenuOpened, setIsBurgerMenuOpened] = useState(false);
+	const [isBurgerMenuOpened, setIsBurgerMenuOpened] = useState(false);
+	const [scrollLocked, setScrollLocked] = useScrollLock();
 
-  return (
-    <>
-      {isBurgerMenuOpened ? (
-        <BurgerMenu setIsBurgerMenuOpened={setIsBurgerMenuOpened} />
-      ) : null}
-      <Header setIsBurgerMenuOpened={setIsBurgerMenuOpened} />
-      <Component {...pageProps} />
-      <Footer />
-    </>
-  );
+	useEffect(() => {
+		isBurgerMenuOpened ? setScrollLocked(true) : setScrollLocked(false);
+	}, [isBurgerMenuOpened]);
+
+	return (
+		<>
+			{isBurgerMenuOpened ? (
+				<BurgerMenu setIsBurgerMenuOpened={setIsBurgerMenuOpened} />
+			) : null}
+			<Header setIsBurgerMenuOpened={setIsBurgerMenuOpened} />
+			<Component {...pageProps} />
+			<Footer />
+		</>
+	);
 }
 
 export default MyApp;
