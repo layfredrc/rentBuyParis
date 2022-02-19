c319b1b632c7873418cadc3a7e2749913a314625
vincent <vincent.n@hotmail.fr>
2022-02-19 16:00:56 +0100

    [AccomodationsHub] draft data ++

--- src/components/accomodationsHub/index.js
+++ src/components/accomodationsHub/index.js
@@ -4,16 +4,63 @@ import HorizontalCard from "../card/horizontal";
 import VerticalCard from "../card/vertical";
 import styles from "./index.module.css";
 
-const data = {
-	src: "https://images.unsplash.com/photo-1502672260266-1c1ef2d93688?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1980&q=80",
-	name: "Joséphine",
-	arrondissement: "15",
-	street: "10 rue de Rivoli 75015",
-	isDisponible: true,
-	nbRooms: 2,
-	size: 30,
-	price: 1500,
-};
+const data = [
+	{
+		id: 1,
+		src: "https://images.unsplash.com/photo-1502672260266-1c1ef2d93688?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1980&q=80",
+		name: "Joséphine",
+		arrondissement: "15",
+		street: "10 rue de Rivoli 75015",
+		isDisponible: true,
+		nbRooms: 2,
+		size: 30,
+		price: 1500,
+	},
+	{
+		id: 2,
+		src: "https://images.unsplash.com/photo-1502672260266-1c1ef2d93688?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1980&q=80",
+		name: "Joséphine",
+		arrondissement: "15",
+		street: "10 rue de Rivoli 75015",
+		isDisponible: true,
+		nbRooms: 2,
+		size: 30,
+		price: 1500,
+	},
+	{
+		id: 3,
+		src: "https://images.unsplash.com/photo-1502672260266-1c1ef2d93688?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1980&q=80",
+		name: "Joséphine",
+		arrondissement: "15",
+		street: "10 rue de Rivoli 75015",
+		isDisponible: true,
+		nbRooms: 2,
+		size: 30,
+		price: 1500,
+	},
+	{
+		id: 4,
+		src: "https://images.unsplash.com/photo-1502672260266-1c1ef2d93688?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1980&q=80",
+		name: "Joséphine",
+		arrondissement: "15",
+		street: "10 rue de Rivoli 75015",
+		isDisponible: true,
+		nbRooms: 2,
+		size: 30,
+		price: 1500,
+	},
+	{
+		id: 5,
+		src: "https://images.unsplash.com/photo-1502672260266-1c1ef2d93688?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1980&q=80",
+		name: "Joséphine",
+		arrondissement: "15",
+		street: "10 rue de Rivoli 75015",
+		isDisponible: true,
+		nbRooms: 2,
+		size: 30,
+		price: 1500,
+	},
+];
 
 export default function AccomodationsHub() {
 	const isMobile = useSizeScreen(600);
@@ -26,10 +73,12 @@ export default function AccomodationsHub() {
 				<div className={styles.content}>
 					<div className={styles.filters}></div>
 					<div className={styles.accomodations}>
-						{!isMobile ? (
-							<HorizontalCard data={data} />
-						) : (
-							<VerticalCard data={data} />
+						{data.map((d) =>
+							!isMobile ? (
+								<HorizontalCard data={d} />
+							) : (
+								<VerticalCard data={d} />
+							)
 						)}
 					</div>
 				</div>
