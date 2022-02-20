4d28fd3984c3974046f87811b513cff70a82045a
vincent <vincent.n@hotmail.fr>
2022-02-21 00:10:07 +0100

    [ACCOMODATIONS] style and filters

--- src/components/accomodationsHub/index.js
+++ src/components/accomodationsHub/index.js
@@ -1,10 +1,12 @@
 import useSizeScreen from "../../lib/use/useSizeScreen";
-import Button from "../button";
+import CustomButton from "../button";
 import CalendarDates from "../calendar";
 import styles from "./index.module.css";
 
 import { MdFilterAlt } from "react-icons/md";
 import Card from "../card";
+import Filter from "../filter";
+import { Checkbox, NumberInput, RangeSlider } from "@mantine/core";
 
 const data = [
 	{
@@ -65,7 +67,12 @@ const data = [
 ];
 
 export default function AccomodationsHub() {
-	const isMobile = useSizeScreen(600);
+	const isMobile = useSizeScreen(768);
+
+	const marks = [
+		{ value: 0, label: "0€" },
+		{ value: 100, label: "100k€" },
+	];
 
 	return (
 		<div className={styles.container}>
@@ -74,17 +81,88 @@ export default function AccomodationsHub() {
 				<CalendarDates title="Quand voulez-vous venir ?" />
 				<div className={styles.content}>
 					<div className={styles.filters}>
-						{isMobile ? (
-							<Button
+						{isMobile && (
+							<CustomButton
 								title="Appliquer des filtres"
 								padding="15px"
 								icon={<MdFilterAlt color="white" size={18} />}
 							/>
-						) : (
-							<></>
+						)}
+						{!isMobile && (
+							<>
+								<Filter title="Quartier">
+									<div style={{ display: "flex" }}>
+										<NumberInput defaultValue={0} required hideControls />
+										<span
+											style={{ minWidth: "max-content", alignSelf: "center" }}
+										>
+											ème arr (Trocadéro)
+										</span>
+									</div>
+								</Filter>
+								<Filter title="Prix">
+									<RangeSlider
+										defaultValue={[20, 80]}
+										marks={marks}
+										color="#3EA6C7"
+										label={(value) => `${value}k €`}
+										styles={(theme) => ({
+											label: {
+												backgroundColor: theme.white,
+												color: theme.black,
+											},
+										})}
+									/>
+								</Filter>
+								<Filter title="Capacité">
+									<Checkbox
+										checked={false}
+										label="1 personne"
+										style={{ marginBottom: "10px" }}
+									/>
+									<Checkbox
+										checked={false}
+										label="2 personnes"
+										style={{ marginBottom: "10px" }}
+									/>
+									<Checkbox
+										checked={false}
+										label="3 personnes"
+										style={{ marginBottom: "10px" }}
+									/>
+									<Checkbox
+										checked={false}
+										label="4 personnes"
+										style={{ marginBottom: "10px" }}
+									/>
+								</Filter>
+								<Filter title="Général">
+									<Checkbox
+										checked={false}
+										label="Parking"
+										style={{ marginBottom: "10px" }}
+									/>
+									<Checkbox
+										checked={false}
+										label="Ascenseur"
+										style={{ marginBottom: "10px" }}
+									/>
+									<Checkbox
+										checked={false}
+										label="Animaux autorisés"
+										style={{ marginBottom: "10px" }}
+									/>
+									<Checkbox
+										checked={false}
+										label="Terrasses / Balcons"
+										style={{ marginBottom: "10px" }}
+									/>
+								</Filter>
+							</>
 						)}
 					</div>
 					<div className={styles.accomodations}>
+						<div className={styles.decoration} />
 						{data.map((d) => (
 							<Card data={d} key={d.id} />
 						))}
--- src/components/button/index.js
+++ src/components/button/index.js
@@ -1,11 +1,11 @@
-import { useState } from "react";
 import styles from "./index.module.scss";
 
 export default function Button({ title, icon, padding }) {
 	return (
 		<div
 			className={styles.container}
-			style={padding ? { padding: padding } : {}}>
+			style={padding ? { padding: padding } : {}}
+		>
 			{icon ? icon : null}
 			<p style={icon && { marginLeft: "5px" }}>{title}</p>
 		</div>
