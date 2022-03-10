fdb4c2b2d2487c887ce83ed5bcc94a4674ec4c92
layfredrc <47161258+layfredrc@users.noreply.github.com>
2022-03-10 15:45:37 +0100

    Popup on Notification buy page

--- src/components/accomodationsHub/index.js
+++ src/components/accomodationsHub/index.js
@@ -1,5 +1,5 @@
 import { useRouter } from "next/router";
-import { Button, Notification } from "@mantine/core";
+import { Button, Notification, Modal, Group } from "@mantine/core";
 import { MdInfo } from "react-icons/md";
 import { useState, useEffect } from "react";
 
@@ -7,13 +7,25 @@ import { useState, useEffect } from "react";
 import Card from "../card";
 import Filters from "../filters";
 import CalendarDates from "../calendar";
-
+import Image from "next/image"
 // Style
+import styled from "styled-components";
 import styles from "./index.module.css";
+import stylesPopup from "../burgerMenu/index.module.scss"
+import popupImage from "../../../public/assets/images/popup-image.jpg";
+import { AiOutlineMail } from "react-icons/ai";
+import { BsTelephone } from "react-icons/bs";
+import whatsapp from "../../../public/assets/images/WhatsApp.png";
+import viber from "../../../public/assets/images/Viber.png";
+import telegram from "../../../public/assets/images/Telegram.png";
+import signal from "../../../public/assets/images/Signal.png";
+import youtube from "../../../public/assets/images/Youtube.png";
 
 export default function AccomodationsHub({ accomodations }) {
 	const router = useRouter();
 	const property = router.query.property;
+	const [opened, setOpened] = useState(false);
+
 
 	const [filteredAccomodations, setFilteredAccomodations] = useState([]);
 	const [district, setDistrict] = useState(null);
@@ -89,17 +101,60 @@ export default function AccomodationsHub({ accomodations }) {
 								New offers will be published. Are you actively looking to buy or
 								sell a property?
 							</p>
-							<Button
-								color="dark"
-								radius={0}
-								size="md"
-								style={{
-									padding: "10px 30px",
-									fontFamily: "'Montserrat',sans-serif",
-								}}
-							>
-								Contact Me
-							</Button>
+							<Group position='center'>
+								<Modal
+									opened={opened}
+									size='90%'
+									onClose={() => setOpened(false)}
+									hideCloseButton
+									padding={0}
+									radius={0}>
+									<Popup>
+										<Image src={popupImage}></Image>
+
+										<div
+											className={stylesPopup.contact}
+											style={{ padding: "2rem", animation: "none" }}>
+											<p className={styles.brand}>RentBuyParis</p>
+											<h2 style={{ fontSize: "20px", fontWeight: "700" }}>
+												Stephan Jaquet
+											</h2>
+											<div className={stylesPopup.information}>
+												<BsTelephone size={20} />
+												<span>
+													<a href='tel:06-72-64-44-44'>+33 6 72 64 44 44</a>
+												</span>
+											</div>
+											<div className={stylesPopup.information}>
+												<AiOutlineMail size={20} />
+												<span> info@rentbuyparis.com</span>
+											</div>
+											<div className={stylesPopup.social}>
+												<Image src={telegram} alt='telegram' />
+												<Image src={youtube} alt='youtube' />
+												<Image src={whatsapp} alt='whatsapp' />
+												<Image src={viber} alt='viber' />
+												<Image src={signal} alt='signal' />
+											</div>
+											<div></div>
+											<p>Please send a SMS if I am not answering</p>
+											<p>
+												47 rue de Laborde, 75008 Paris Mon – Fri: 11:00 — 18:00
+												(CET), Sat – Sun: available for a call
+											</p>
+										</div>
+									</Popup>
+								</Modal>
+								<Button
+									color='dark'
+									style={{
+										fontFamily: "'Montserrat',sans-serif",
+										marginTop: "1rem",
+									}}
+									onClick={() => setOpened(true)}>
+									Contact Me
+								</Button>
+							</Group>
 						</Notification>
 					</div>
 				) : (
@@ -133,3 +188,11 @@ export default function AccomodationsHub({ accomodations }) {
 		</div>
 	);
 }
+
+
+const Popup = styled.div`
+	@media screen and (min-width: 1024px) {
+		display: flex;
+		justify-content: space-between;
+	}
+`;
