50995ab4b248876d5461cec8a56df56bb26e15b2
layfredrc <47161258+layfredrc@users.noreply.github.com>
2022-04-06 00:34:59 +0200

    [FIX] reset filter on page change

--- src/components/accomodationsHub/index.js
+++ src/components/accomodationsHub/index.js
@@ -106,7 +106,8 @@ export default function AccomodationsHub({ accomodations }) {
 
 	useEffect(() => {
 		applyDatesFilter();
-	}, []);
+		resetFilters();
+	}, [property]);
 
 	const resetFilters = () => {
 		setFilteredAccomodations(accomodations);
